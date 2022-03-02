575d4a7a30cfe994efcc80ee7bfe195a3ebd6c92
minecraftbros999 <bryan.cooper594@gmail.com>
2022-03-03 09:06:21 +1300

    Update Tournament.py
    Program to calculate points for a single team within a tournament

--- Tournament.py
+++ Tournament.py
@@ -3,11 +3,11 @@
 
 # Introduction and getting home team name
 print("Welcome to the Super Tournament Points Calculation system")
-g = True
-while g == True:
+get_team1 = True
+while get_team1 == True:
     team1 = input("Enter the name of the team")
     if team1:
-        g = False
+        get_team1 = False
     else:
         print("Please input a valid team name")
 # Declare oppoenent list and receive all opponent team names
@@ -18,9 +18,14 @@ while keep_asking == True:
     opponent = input("Enter the name of an opponent (if done enter 'done')")
     if opponent.lower() == "done":
         keep_asking = False
+    elif opponent == team1:
+        print("Don't play a game against yourself!")
+    elif len(opponent.replace(" ", ""))==0:
+        print("Please enter a valid opposing team")
     elif opponent:
         # Checking the input isn't empty
         opponent_list.append(opponent)
+    
     else:
         print("Please enter a valid opposing team")
 # Results
@@ -30,9 +35,9 @@ length = len(opponent_list)
 # Print all results, increeasing the home team's total points based on the outcome of the game
 for i in range(0,length):
     print(f"Game versus {opponent_list[i]}")
-    score1_test = True
     negative = True
     while negative == True:
+        score1_test = True
         while score1_test == True:
             score1_test = False
             try:
@@ -46,15 +51,13 @@ for i in range(0,length):
             print("Enter a valid integer")
         else:
             negative = False   
-            
-    score2_test = True
     negative2 = True
     while negative2 == True:
+        score2_test = True
         while score2_test == True:
             score2_test = False
             try:
-                score1 = int(input(f"{team1} score:"))
-            
+                score2 = int(input(f"{opponent_list[i]} score:"))
             except ValueError:
                 # Making sure that the input is a number
                 print("Enter a valid integer")
@@ -77,7 +80,11 @@ for i in range(0,length):
         print("Error")
 # Print final stats
 if length == 0:
+    # Checking for if there are no opposing teams
     print("No opposing teams... so no points :)")
+elif team1_score == 1:
+    print("Competition complete!")
+    print(f"{team1} finished the competition with {team1_score} point.")
 else:
     print("Competition complete!")
     print(f"{team1} finished the competition with {team1_score} points.")
