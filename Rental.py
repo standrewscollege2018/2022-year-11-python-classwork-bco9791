ac56a1375c26ad7c7450a388ceab408a0aaa1a1f
minecraftbros999 <bryan.cooper594@gmail.com>
2022-03-11 10:49:53 +1300

    Update Rental.py
    Set up a list to check if a vehicle is unavailable. Removed confirmation check because it was causing too many issues.

--- Rental.py
+++ Rental.py
@@ -3,58 +3,50 @@
 print("Welcome to the University vehicle rental system")
 vehicles = [["Suzuki Van", 2], ["Toyota Corolla", 4], ["Honda CRV", 4], ["Suzuki Swift", 4], ["Mitsubishi Airtrek", 4], ["Nissan DC Ute", 4], ["Toyota Previa", 7], ["Toyota Hi Ace", 12], ["Toyota Hi Ace", 12]]
 names = []
-unavailable = ["- available", "- available", "- available", "- available", "- available", "- available", "- available", "- available", "- available", "- available", "- available", "- available"]
 booking_list = []
+unavailable = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 get_vehicles = True
 while get_vehicles == True:
     print("The vehicles are:")
     for i in range(0, 9):
-        print(f"{i+1}. {vehicles[i][0]} ({vehicles[i][1]})")
-    get_vehicle = True
+        print(f"{i+1}. {vehicles[i][0]} ({vehicles[i][1]}) {unavailable[i-1]}")
     get_input = True
     get_name = True
-    get_confirmation = True
-    # Bring all booleans up here
-    while get_vehicle == True:
-        while get_input == True:
-            try:
-                book = int(input("Which vehicle would you like to book? (type 0 to end the process)"))
-                if book > 9 or book < 0:
-                    print("Please enter a valid integer from 0-9")
-                elif book == 0:
-                    get_vehicle = False
-                    get_input = False
-                    get_vehicles = False
-                    get_name = False
-                    # Come back here to disable all loops
-                elif book:
-                    get_input = False
-                    booking_list.append(book}
-                else:
-                    print("Please enter a valid integer from 0-9")
-            except ValueError:
+    while get_input == True:
+         try:
+             book = int(input("Which vehicle would you like to book? (type 0 to end the process)"))
+             if book > 9 or book < 0:
+                 print("Please enter a valid integer from 0-9")
+             elif book == 0:
+                 get_input = False
+                 get_vehicles = False
+                 get_name = False
+                 # Come back here to disable all loops
+             elif unavailable[book - 2] == "- unavailable":
+                 print("** That car is already booked. Please choose another. **")
+             elif book:
+                 get_input = False
+                 unavailable[book - 2] = "- unavailable"
+                 booking_list.append(vehicles[book - 1][0])
+             else:
                 print("Please enter a valid integer from 0-9")
-        print("You have selected the {vehicle[book-1][0]")
-        while get_confirmation == True:
-            select = input("Is this selection correct?")
-            if select.lower == "yes":
-                get_vehicle = False
-                get_confirmation = False
-                unavailable[book-1] = "- unavailable"
-            elif select.lower == "no":
-                get_confirmation = False
-                print("Please select another vehicle")
-            else:
-                print("Please enter yes or no")
+         except ValueError:  
+             print("Please enter a valid integer from 0-9")
+    print(f"You have selected the {vehicles[book - 1][0]}")    
     while get_name == True:
         name = input("Please enter your name")
         if name:
             if name.isalpha():
-                get_name = False
-                print(f"Thank you {name}")
-                names.append(name)
+                if name not in names:
+                    get_name = False
+                    print(f"Thank you {name}")
+                    names.append(name)
+                else:
+                    print("One car per person please")
+            else:
+                print("Please enter a valid name")
         else:
             print("Please enter a valid name")
 print("Daily Summary")
-for i in range (1, len(names))
-print(f"{vehicles[booking_list[i-1][0]} - {names[i-1]")
+for i in range (1, len(names)):
+    print(f"booking_list[i-1]} - {names[i-1]}")
